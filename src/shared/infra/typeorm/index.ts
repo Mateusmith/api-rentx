26516e98775c3c86c57ec043d5ed74b24a7a91bf
mateus <mateus@Smith>
2022-07-06 19:14:56 -0300

    Rate Limiter

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,7 +7,7 @@ export default async (): Promise<Connection> => {
     Object.assign(defaultOptions, {
       database:
         process.env.NODE_ENV === "test"
-          ? "rentx"
+          ? "rentx_test"
           : defaultOptions.database,
     })
   );
