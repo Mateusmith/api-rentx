18260640478496f9d4dce0240504211c4e415fdc
mateus <mateus@Smith>
2022-12-16 16:59:09 -0300

    rentx007

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { RateLimiterRedis } from "rate-limiter-flexible";
 import * as redis from "redis";
 
@@ -9,14 +9,13 @@ const redisClient = redis.createClient({
   socket: {
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
-    sessionTimeout: 20,
   },
 });
 
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: "rateLimiter",
-  points: 10,
+  points: 5,
   duration: 5,
 });
 
@@ -26,14 +25,10 @@ export default async function rateLimiter(
   next: NextFunction
 ): Promise<void> {
   try {
-    await redisClient.connect();
-
     await limiter.consume(request.ip);
-
     return next();
-  } catch (e) {
+  } catch (err) {
+    console.log(err);
     throw new AppError("Too many requests", 429);
-  } finally {
-    await redisClient.disconnect();
   }
 }
